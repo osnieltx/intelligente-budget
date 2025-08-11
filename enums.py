12177ae03262e86a914b8a2a50fdcbdf73ef75f0
Osniel Teixeira <osnielteixeira@icloud.com>
2025-08-11 18:54:09 -0300

    consuming transactions from nubank cc statement

---
+++ enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class AccountType(Enum):
+    BANK = 0
+    CREDIT = 1
--- nubank.py
+++ nubank.py
@@ -1,25 +1,28 @@
 import pluggy_sdk
-from pprint import pprint
 
 from datetime import datetime
 
 import settings
+from enums import AccountType
 
 configuration = pluggy_sdk.Configuration()
 configuration.api_key['default'] = settings.API_KEY
 
 
-def transactions(from_date: datetime, to_date: datetime):
+def transactions(from_date: datetime, to_date: datetime, account: AccountType):
     with pluggy_sdk.ApiClient(configuration) as api_client:
         transactions_api = pluggy_sdk.TransactionApi(api_client)
+        if account is AccountType.BANK:
+            account_id = settings.ACCOUNT_ID
+        elif account is AccountType.CREDIT:
+            account_id = settings.ACCOUNT_ID_CC
         page_size = 50
         page = 1
-
         items = []
         try:
             while True:
                 api_response = transactions_api.transactions_list(
-                    settings.ACCOUNT_ID,
+                    account_id,
                     var_from=from_date,
                     to=to_date,
                     page_size=page_size,
